62f5de1aab48e6b21374d18c0aaa915b82824207
Giampaolo Guiducci <stirner@gmail.com>
2019-05-25 11:00:32 +0200

    emacs: moved .emacs to .emacs.d/init.el

--- dotfiles/emacs.d/init.el
+++ dotfiles/emacs.d/init.el
@@ -188,7 +188,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (evil))))
+ '(package-selected-packages (quote (tidal evil))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
