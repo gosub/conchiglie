86f6af70539f8ea027462460779b37cf10451638
Giampaolo Guiducci <stirner@gmail.com>
2019-05-27 11:24:48 +0200

    emacs: customizations extracted from init.el to separate file

--- dotfiles/emacs.d/init.el
+++ dotfiles/emacs.d/init.el
@@ -95,18 +95,3 @@
                       (1+ (string-to-number (match-string 0)))))))
 
 (global-set-key (kbd "C-c +") 'increment-number-at-point)
-
-;; added by emacs
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (tidal evil))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:family "Source Code Pro" :foundry "ADBO" :slant normal :weight normal :height 113 :width normal)))))
