d4b5d1104535b8f4b6e886c0e0e4015ad09c2b56
Giampaolo Guiducci <stirner@gmail.com>
2019-05-31 15:59:52 +0200

    emacs: installed csound-mode and magit

--- dotfiles/emacs.d/custom.el
+++ dotfiles/emacs.d/custom.el
@@ -5,5 +5,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (tidal evil))))
+ '(package-selected-packages (quote (magit tidal evil csound-mode))))
 
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
