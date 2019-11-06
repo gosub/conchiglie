252e5427d3a1f62175d297ca36c7c6d286e4dc87
Giampaolo Guiducci <giampaolo.guiducci@gmail.com>
2019-11-06 10:44:14 +0100

    emacs: added 'ASIDE' tag with dark blue color

--- dotfiles/emacs.d/cfg/gg-org.el
+++ dotfiles/emacs.d/cfg/gg-org.el
@@ -4,5 +4,10 @@
 
 (add-hook 'org-mode-hook 'gg/org-mode-hook)
 
+(setq org-todo-keywords
+      '((sequence "TODO" "ASIDE" "DONE")))
+
+(setq org-todo-keyword-faces
+      '(("ASIDE" . "dark blue")))
 
 (provide 'gg-org)
