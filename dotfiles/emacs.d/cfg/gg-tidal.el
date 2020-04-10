ba0cc0ecebcd248b7b062091fd9fda8f00b19334
Giampaolo Guiducci <giampaolo.guiducci@gmail.com>
2020-04-10 17:24:07 +0200

    emacs: workaround for using tidal from aur

--- dotfiles/emacs.d/cfg/gg-tidal.el
+++ dotfiles/emacs.d/cfg/gg-tidal.el
@@ -20,4 +20,26 @@
 
 (add-hook 'tidal-mode-hook 'gg/tidal-mode-map)
 
+;; redefine path for BootTidal.el, not installed via cabal
+(require 'subr-x)
+(setq tidal-boot-script-path
+  (let ((filepath
+         (cond
+          ((string-equal system-type "windows-nt")
+           '(("path" . "echo off && for /f %a in ('ghc-pkg latest tidal') do (for /f \"tokens=2\" %i in ('ghc-pkg describe %a ^| findstr data-dir') do (echo %i))")
+             ("separator" . "\\")
+             ))
+          ((or (string-equal system-type "darwin") (string-equal system-type "gnu/linux"))
+           '(("path" . "ghc-pkg field tidal data-dir")
+             ("separator" . "/")
+             ))
+          )
+         ))
+    (concat
+     (string-trim (cadr (split-string
+                         (shell-command-to-string (cdr (assoc "path" filepath))) ":")))
+     (cdr (assoc "separator" filepath))
+     "BootTidal.hs")))
+
+
 (provide 'gg-tidal)
