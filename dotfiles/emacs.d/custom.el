6ef3417cfcdac60f955fc09b0c0a3da787c3cbe0
Giampaolo Guiducci <giampaolo.guiducci@gmail.com>
2022-05-22 12:21:40 +0200

    emacs: he did this by itself :/

--- dotfiles/emacs.d/custom.el
+++ dotfiles/emacs.d/custom.el
@@ -5,7 +5,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (tidal magit ledger-mode evil csound-mode))))
+ '(package-selected-packages '(tidal magit ledger-mode evil csound-mode)))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
