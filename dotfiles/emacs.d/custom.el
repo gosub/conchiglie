291a6fac71507b7b0d858d38959e8a49c652d832
Giampaolo Guiducci <giampaolo.guiducci@gmail.com>
2022-08-19 11:06:11 +0200

    emacs: ha fatto tutto lui in custom.el

--- dotfiles/emacs.d/custom.el
+++ dotfiles/emacs.d/custom.el
@@ -5,7 +5,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(tidal magit ledger-mode evil csound-mode)))
+ '(package-selected-packages
+   '(ox-hugo undo-tree tidal magit ledger-mode evil csound-mode async)))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
