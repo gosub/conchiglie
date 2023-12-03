63c06d6dd6ae80061d43f92a9ccdd06cc34298d9
Giampaolo Guiducci <giampaolo.guiducci@gmail.com>
2023-12-03 17:59:34 +0100

    emacs: one day I'll get rid of the custom file, promise.

--- home/emacs/.emacs.d/custom.el
+++ home/emacs/.emacs.d/custom.el
@@ -5,8 +5,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(move-text undo-tree tidal ox-hugo magit ledger-mode evil csound-mode async)))
+ '(package-selected-packages '(magit with-editor ledger-mode)))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
